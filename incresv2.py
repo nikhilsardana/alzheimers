67c4d146401f2c1b65195acd45e8c2f940995426
nikhilsardana <2018nsardana@tjhsst.edu>
2017-10-24 21:35:15 -0400

    keras multi-gpu

--- incresv2.py
+++ incresv2.py
@@ -9,6 +9,9 @@ from keras.layers.core import Flatten
 from keras.callbacks import ModelCheckpoint
 from keras.applications.inception_resnet_v2 import InceptionResNetV2
 from sklearn.metrics import classification_report
+import mxnet as mx
+import argparse
+
 
 train_data_dir = os.getcwd() + "/../imgdata/train"
 validation_data_dir = os.getcwd() + "/../imgdata/test"
@@ -16,7 +19,9 @@ validation_data_dir = os.getcwd() + "/../imgdata/test"
 nb_train_samples = 2029440
 nb_validation_samples = 678720
 epochs = 5
-batch_size = 32
+batch_per_gpu = 64
+num_gpus = 4
+batch_size = num_gpus * batch_per_gpu
 
 train_datagen = ImageDataGenerator(rescale=1./255)
 
@@ -40,13 +45,28 @@ model = InceptionResNetV2(include_top=True, weights=None, classes=5)
 
 #categorical classifier
 
+
+def backend_agnostic_compile(model, loss, optimizer, metrics, args):
+    if keras.backend._backend == 'mxnet':
+        gpu_list = ["gpu(%d)" % i for i in range(num_gpus)]
+        model.compile(loss=loss,
+            optimizer=optimizer,
+            metrics=metrics, 
+            context = gpu_list)
+    else:
+        if num_gpus > 1:
+            print("Warning: num_gpus > 1 but not using MxNet backend")
+        model.compile(loss=loss,
+            optimizer=optimizer,
+            metrics=metrics)
+
 sgd = SGD(lr=0.01, decay=1e-6, momentum=0.9, nesterov=True)
-model.compile(loss='categorical_crossentropy',
-              optimizer=sgd,
-              metrics=['accuracy'])
 
+backend_agnostic_compile(
+    model=model, loss='categorical_crossentropy', 
+    optimizer=sgd, metrics=['accuracy'], args=args)
 
-filepath="weights-improvement-{epoch:02d}-{val_acc:.2f}.hdf5"
+filepath="weight-improvement-{epoch:02d}-{val_acc:.2f}.hdf5"
 checkpoint = ModelCheckpoint(filepath, monitor='val_acc', verbose=1, save_best_only=True, mode='max')
 callbacks_list = [checkpoint]
 
@@ -58,7 +78,7 @@ model.fit_generator(
         validation_data=validation_generator,
         validation_steps=nb_validation_samples // batch_size)
 
-model.save_weights('transfer_model.h5')
+model.save_weights('incresv2_model.h5')
 
 predictions = model.predict_generator(validation_generator, steps=678720//32)
 print(predictions)
